7724bae5f7e28b135b77882802ca0bcfe782d467
Khushal Agarwal <hrithik.agarwal87@gmail.com>
2019-08-09 10:34:50 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,35 +1,35 @@
-////By KHUSHAL AGARWAL
-//import java.util.Scanner;
-//public class input 
-//{
-//    int getInput()
-//    {
-//        Scanner obj=new Scanner(System.in);
-//        System.out.println("Enter number - ");
-//        int num=obj.nextInt();
-//        return num;
-//    }
-//    void check(int n)
-//    {
-//        if(n%2==0)
-//        {
-//            System.out.println("Even number");
-//        }
-//        else
-//        {
-//            System.out.println("Odd number");
-//        }
-//    }
-//    public static void main(String args[])
-//    {
-//        input obj=new input();
-//        int check=obj.getInput();
-//        obj.check(check);
-//        
-//    }
-//}
-//
-////Output -
-////Enter number - 
-////10
-////Even number
+//By KHUSHAL AGARWAL
+import java.util.Scanner;
+public class input 
+{
+   int getInput()
+   {
+       Scanner obj=new Scanner(System.in);
+       System.out.println("Enter number - ");
+       int num=obj.nextInt();
+       return num;
+   }
+   void check(int n)
+   {
+       if(n%2==0)
+       {
+           System.out.println("Even number");
+       }
+       else
+       {
+           System.out.println("Odd number");
+       }
+   }
+   public static void main(String args[])
+   {
+       input obj=new input();
+       int check=obj.getInput();
+       obj.check(check);
+       
+   }
+}
+
+//Output -
+//Enter number - 
+//10
+//Even number
