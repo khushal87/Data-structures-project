5dbcaed1a8ac7c26129d94c06521828760e5ba55
shuchitpant <56576450+shuchitpant@users.noreply.github.com>
2019-10-17 19:32:28 +0530

    Created a program for Hacktoberfest

---
+++ prog1.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int i=0;
+	    try{
+	    Scanner s= new Scanner(System.in);
+	    while(true)
+	    {
+	        
+	        i=s.nextInt();
+	        if(i==42)
+	        break;
+	        System.out.println(i);
+	        
+	    }
+	    }
+	    catch(Exception e){
+	    }
+	}
+}
